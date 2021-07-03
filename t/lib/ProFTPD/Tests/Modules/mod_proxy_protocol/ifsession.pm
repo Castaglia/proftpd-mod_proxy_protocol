75dfe7c8ead4a14ccdd3f02aa5acededff3f969d
TJ Saunders <tj@castaglia.org>
2021-07-03 10:48:12 -0700

    Adding `mod_proxy_protocol` + `mod_ifsession` regression tests for https://github.com/proftpd/proftpd/issues/1256.

--- t/lib/ProFTPD/Tests/Modules/mod_proxy_protocol/ifsession.pm
+++ t/lib/ProFTPD/Tests/Modules/mod_proxy_protocol/ifsession.pm
@@ -28,6 +28,16 @@ my $TESTS = {
     test_class => [qw(forking mod_ifsession mod_proxy_protocol)],
   },
 
+  proxy_protocol_ifsess_matching_user => {
+    order => ++$order,
+    test_class => [qw(forking mod_ifsession mod_proxy_protocol)],
+  },
+
+  proxy_protocol_ifsess_mismatched_user => {
+    order => ++$order,
+    test_class => [qw(forking mod_ifsession mod_proxy_protocol)],
+  },
+
   proxy_protocol_config_ignore_ifsess_matching_class_issue2 => {
     order => ++$order,
     test_class => [qw(forking mod_ifsession mod_proxy_protocol)],
@@ -208,6 +218,191 @@ EOC
       my $expected = 5;
       $self->assert($resp_code == $expected,
         test_msg("Expected response code $expected, got $resp_code"));
+    };
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh, 10) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+  $self->assert_child_ok($pid);
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub proxy_protocol_ifsess_matching_user {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'ifsess');
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+
+    AuthUserFile => $setup->{auth_user_file},
+    AuthGroupFile => $setup->{auth_group_file},
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  if (open(my $fh, ">> $setup->{config_file}")) {
+    print $fh <<EOC;
+<IfModule mod_proxy_protocol.c>
+  ProxyProtocolEngine on
+</IfModule>
+
+<IfModule mod_ifsession.c>
+  <IfUser $setup->{user}>
+    <Limit LOGIN>
+      Deny from 1.1.1.1
+    </Limit>
+  </IfUser>
+</IfModule>
+EOC
+    unless (close($fh)) {
+      die("Can't write $setup->{config_file}: $!");
+    }
+
+  } else {
+    die("Can't open $setup->{config_file}: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      sleep(2);
+
+      my $client = ProFTPD::TestSuite::ProxiedFTP->new('127.0.0.1', $port,
+        ['TCP4', '1.1.1.1', '2.2.2.2', 111, 222]);
+      eval { $client->login($setup->{user}, $setup->{passwd}); };
+      unless ($@) {
+        die("Login succeeded unexpectedly");
+      }
+      $client->quit();
+    };
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($setup->{config_file}, $rfh, 10) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($setup->{pid_file});
+  $self->assert_child_ok($pid);
+
+  test_cleanup($setup->{log_file}, $ex);
+}
+
+sub proxy_protocol_ifsess_mismatched_user {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+  my $setup = test_setup($tmpdir, 'ifsess');
+
+  my $config = {
+    PidFile => $setup->{pid_file},
+    ScoreboardFile => $setup->{scoreboard_file},
+    SystemLog => $setup->{log_file},
+
+    AuthUserFile => $setup->{auth_user_file},
+    AuthGroupFile => $setup->{auth_group_file},
+
+    IfModules => {
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($setup->{config_file},
+    $config);
+
+  if (open(my $fh, ">> $setup->{config_file}")) {
+    print $fh <<EOC;
+<IfModule mod_proxy_protocol.c>
+  ProxyProtocolEngine on
+</IfModule>
+
+<IfModule mod_ifsession.c>
+  <IfUser foobar>
+    <Limit LOGIN>
+      Deny from 1.1.1.1
+    </Limit>
+  </IfUser>
+</IfModule>
+EOC
+    unless (close($fh)) {
+      die("Can't write $setup->{config_file}: $!");
+    }
+
+  } else {
+    die("Can't open $setup->{config_file}: $!");
+  }
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      sleep(2);
+
+      my $client = ProFTPD::TestSuite::ProxiedFTP->new('127.0.0.1', $port,
+        ['TCP4', '1.1.1.1', '2.2.2.2', 111, 222]);
+      $client->login($setup->{user}, $setup->{passwd});
+      $client->quit();
     };
     if ($@) {
       $ex = $@;
